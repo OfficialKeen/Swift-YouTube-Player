c1ae92a6670654f39bc0298849732a05a7d9340a
Keen <zhekeen90@gmail.com>
2025-05-06 10:58:34 +0700

    Package

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "YouTubePlayer",
+    platforms: [
+        .iOS(.v13) // Tentukan versi minimum deployment target yang kamu inginkan
+    ],
+    dependencies: [
+        // Menambahkan dependensi Swift-YouTube-Player
+        .package(url: "https://github.com/gilesvangruisen/Swift-YouTube-Player", from: "2.0.0")
+    ],
+    products: [
+        .library(
+            name: "YouTubePlayer",
+            targets: ["YouTubePlayer"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "YouTubePlayer",
+            dependencies: [
+                // Pastikan menambahkan dependensi ini ke target
+                .product(name: "SwiftYouTubePlayer", package: "Swift-YouTube-Player")
+            ],
+            path: "YouTubePlayer"
+        ),
+    ]
+)
